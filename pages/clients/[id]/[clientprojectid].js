2e2d63c5fdc5a2d3a236a3b8caf6b603447ae010
miguelii <miguelgoncalves18@hotmail.com>
2022-09-28 16:44:29 +0100

    File Router

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,10 @@
+
+function SlectedClientrojectPage() {
+    return (
+        <div>
+            <h1> The project page for a specific project for a select client</h1>
+        </div>
+    )
+}
+
+export default SlectedClientrojectPage;
---
+++ pages/portfolio/[id].js
@@ -0,0 +1,15 @@
+import {useRouter} from 'next/router'
+
+function PortfolioPageID() {
+    const router = useRouter();
+
+    
+
+    return (
+        <div>
+            <h1> Portfolio Page ID !!</h1>
+        </div>
+    )
+}
+
+export default PortfolioPageID;
