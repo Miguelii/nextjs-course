c5ba218d734560d5d5e97b719244ee400860f595
miguelii <miguelgoncalves18@hotmail.com>
2022-10-06 18:12:29 +0100

    Section-10 end

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,25 @@
-import { Fragment } from 'react';
-
+import { Fragment, useContext } from 'react';
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 function Layout(props) {
+
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotificaiton = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotificaiton && ( 
+      <Notification 
+        title={activeNotificaiton.title} 
+        message={activeNotificaiton.message} 
+        status={activeNotificaiton.status} 
+        /> 
+      )}
     </Fragment>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,20 @@ import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import { NotificaionContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificaionContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='NextJS Events' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificaionContextProvider>
   );
 }
 
